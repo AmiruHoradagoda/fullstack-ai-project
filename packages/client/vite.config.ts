14a959d0fa36bd14cb53d12c8c6d672f2ea88863
AmiruHoradagoda <amirumithsara1234@gmail.com>
2025-09-13 23:39:01 +0530

    Set up Prettier

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,22 +1,22 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  css: {
-    devSourcemap: true, 
-  },
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000",
-    },
-  },
+   plugins: [react(), tailwindcss()],
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
+      },
+   },
+   css: {
+      devSourcemap: true,
+   },
+   server: {
+      proxy: {
+         '/api': 'http://localhost:3000',
+      },
+   },
 });
