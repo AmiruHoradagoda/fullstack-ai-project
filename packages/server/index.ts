c88dffb6a4f940b0e62add11e44acd5bd97a1f15
AmiruHoradagoda <amirumithsara1234@gmail.com>
2025-09-17 21:17:48 +0530

    apply zod to validate input request body

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
-
+import z from 'zod';
 dotenv.config();
 
 const app = express();
@@ -12,21 +12,32 @@ const port = process.env.PORT || 3000;
 
 const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-app.get('/', (req: Request, res: Response) => {
-   res.send('Hellow World!!!!');
-});
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hellow World diffrent' });
+const conversations = new Map<string, string>();
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required.')
+      .max(1000, 'Promt is too long (max 1000 characters) '),
+   conversationId: z.string().uuid(),
 });
 
 app.post('/api/chat', async (req: Request, res: Response) => {
-   const { prompt } = req.body;
+   const parseResult = chatSchema.safeParse(req.body);
+   if (!parseResult.success) {
+      res.status(400).json(parseResult.error.format());
+      return;
+   }
+   const { prompt, conversationId } = req.body;
    const response = await client.responses.create({
       model: 'gpt-4o-mini',
       input: prompt,
       temperature: 0.2,
       max_output_tokens: 100,
+      previous_response_id: conversations.get(conversationId),
    });
+   conversations.set(conversationId, response.id);
    res.json({ message: response.output_text });
 });
 
