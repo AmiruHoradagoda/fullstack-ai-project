9e529984943b1b22473912454adda1c6839e845f
AmiruHoradagoda <amirumithsara1234@gmail.com>
2025-09-20 18:55:54 +0530

    sepereat the chay service to service directory

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,17 +3,13 @@ import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
 import z from 'zod';
-import { conversationRepository } from './repositories/conversation.repository';
+import { chatService } from './services/chat.service';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
-
-const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-
-const conversations = new Map<string, string>();
 
 const chatSchema = z.object({
    prompt: z
@@ -32,16 +28,8 @@ app.post('/api/chat', async (req: Request, res: Response) => {
    }
    try {
       const { prompt, conversationId } = req.body;
-      const response = await client.responses.create({
-         model: 'gpt-4o-mini',
-         input: prompt,
-         temperature: 0.2,
-         max_output_tokens: 100,
-         previous_response_id:
-            conversationRepository.getLastResponseId(conversationId),
-      });
-      conversationRepository.setLastResponseId(conversationId, response.id);
-      res.json({ message: response.output_text });
+      const response = await chatService.sendMessage(prompt, conversationId);
+      res.json({ message: response.message });
    } catch (error) {
       res.status(500).json({
          message: 'Faild to generate response',
